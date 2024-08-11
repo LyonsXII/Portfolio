e09588d8b533e30c11d96cf5b51d83db9520f0e3
Michael Lyons <michael.lyons1112@gmail.com>
2024-08-11 20:30:18 +0100

    Beginning to implement themes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,35 @@
 import React, { useState } from "react";
+import { ThemeProvider } from "react-ui";
 import axios from "axios";
 import './App.css'
 
 import Settings from "./Settings";
 
 function App() {
+  const theme = {
+    colors: {
+      primary: '#0070f3',
+      bg: '#fff',
+      text: '#333',
+      grey: '#aaa',
+    },
+    fontSize: {
+      small: '12px',
+      medium: '16px',
+      large: '24px',
+    },
+  };
 
   return (
-    <div className="container">
-      <Settings></Settings>
-      <h1>Portfolio</h1>
-      <h2>Michael Lyons</h2>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="container">
+        <div className="containerBackground">
+        </div>
+        <Settings></Settings>
+        <h1>Portfolio</h1>
+        <h2>Michael Lyons</h2>
+      </div>
+    </ThemeProvider>
   )
 }
 
