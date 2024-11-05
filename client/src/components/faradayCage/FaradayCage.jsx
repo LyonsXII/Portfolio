652284f0ffe47aec4b52e9342226796a8425c383
Michael Lyons <michael.lyons1112@gmail.com>
2024-11-05 19:37:13 +0000

    Adding basic concept of button controls for Faraday Cage

--- client/src/components/faradayCage/FaradayCage.jsx
+++ client/src/components/faradayCage/FaradayCage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import styled from "styled-components";
 import Plot from 'react-plotly.js';
 
@@ -16,14 +16,34 @@ const StyledFlexboxContainer = styled.div`
 `;
 
 const StyledChartContainer = styled.div`
-  height: 80vh;
-  width: 40vw;
+  height: 85vh;
+  width: 50vw;
   border: 4px solid black;
 `;
 
+const StyledButtonContainer = styled.div`
+  height: 100vh;
+  width: 20vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+`;
+
+const StyledRowContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
 const StyledButton = styled.button`
-  height: 80px;
-  width: 10%;
+  width: 40%;
+  padding: 15px 30px;
+  word-wrap: break-word;
+  white-space: normal;
   border: 4px solid black;
   border-radius: 20px;
   color: ${({ theme }) => theme.textColor};
@@ -33,15 +53,37 @@ const StyledButton = styled.button`
   &:hover {
     background-color: ${({ theme }) => theme.secondaryColor};
     color: ${({ theme }) => theme.tertiaryColor};
+    transform: scale(1.01);
+    transition: transform 0.2s ease, background-color 0.8s ease;
+  }
+`;
+
+const StyledIncrementButton = styled.button`
+  width: 10%;
+  padding: 15px 30px;
+  border: 4px solid black;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.primaryColor};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.secondaryColor};
+    color: ${({ theme }) => theme.tertiaryColor};
+    transform: scale(1.01);
+    transition: transform 0.1s ease, background-color 0.8s ease;
   }
 `;
 
 function FaradayCage(props) {
   const { theme } = useContext(ThemeContext);
 
-  const [plotData, setPlotData] = useState({ contour: [] });
+  const [plotData, setPlotData] = useState([]);
   const [axisValues, setAxisValues] = useState({ xValues: [], yValues: [] });
   const [diskValues, setDiskValues] = useState({ diskXValues: [], diskYValues: [] });
+
+  const [numDisks, setNumDisks] = useState(4);
+  const [radiusDisks, setRadiusDisks] = useState(0.1);
+  const [numSides, setNumSides] = useState(360);
 
   // Generate heatmap data from xx, yy, uu
   function updateData(n, r, sides) {
@@ -49,7 +91,7 @@ function FaradayCage(props) {
     let xValues = [];
     let yValues = [];
 
-    const { xx, yy, uu } = Faraday(n, r, sides);
+    const { xx, yy, uu } = Faraday(numDisks, radiusDisks, numSides);
 
     for (let i = 0; i < 120; i++) {
       contour.push([]);
@@ -60,6 +102,7 @@ function FaradayCage(props) {
       }
     }
 
+    // Calculating n-th roots of unity to position disks for plot
     function rootsOfUnity(n) {
       const diskXValues = [];
       const diskYValues = [];
@@ -74,17 +117,24 @@ function FaradayCage(props) {
       return { diskXValues, diskYValues };
     }
 
-    const { diskXValues, diskYValues } = rootsOfUnity(n);
+    const { diskXValues, diskYValues } = rootsOfUnity(numDisks);
 
-    setPlotData({ contour });
+    setPlotData(contour);
     setAxisValues({ xValues, yValues });
     setDiskValues({ diskXValues, diskYValues });
   }
 
-  const n = 8; // Number of disks
-  const r = 0.1; // Radius of disks
-  const sides = 360; // Number of sides for cage
-  
+  function incrementNumDisks(direction) {
+    setNumDisks((prev) => {
+      if (direction === "Add") {
+        return prev + 1;
+      } else if (direction === "Subtract" && prev > 0) {
+        return prev - 1;
+      }
+      return prev;
+    });
+  }
+
   return (
     <StyledFlexboxContainer>
       <StyledChartContainer>
@@ -92,52 +142,91 @@ function FaradayCage(props) {
           data={[
             // Contour plot
             {
-              z: plotData.contour,  // This is the 2D array representing Z values (heights)
+              z: plotData, 
               x: axisValues.xValues,
               y: axisValues.yValues,
-              type: 'contour',  // Specify contour plot
-              colorscale: 'Viridis',  // Choose a colorscale or customize it
+              type: "contour",
+              colorscale: "Portland",
               contours: {
-                coloring: 'lines',  // Options are 'heatmap', 'lines', or 'none'
-                showlines: true,
+                coloring: "lines",  // "heatmap" or "lines"
                 width: 3,
-                line: {
-                  opacity: 0
-                },
                 start: -3.5,
                 end: 1,
                 size: 0.1
-              }
+              },
+              line: {
+                width: 2          // Adjust this for thicker lines
+              },
+              showscale: false
             },
-            // Disks
+            // Plot Disks (Visual Aid Only)
             {
-              x: diskValues.diskXValues,     // X-coordinates of disks
-              y: diskValues.diskYValues,    // Y-coordinates of disks
-              mode: 'markers',
-              type: 'scatter',
+              x: diskValues.diskXValues,
+              y: diskValues.diskYValues,
+              mode: "markers",
+              type: "scatter",
               marker: {
-                size: 22,                // Size of disks (in pixels)
-                color: 'rgba(255, 0, 0, 0.6)',  // Disk color with transparency
+                size: 22,
+                color: "rgba(255, 0, 0, 0.6)",
                 line: {
-                  color: 'black',        // Outline color
+                  color: "black",
                   width: 1,
                 },
-                symbol: 'circle',        // Ensures markers are circular
+                symbol: "circle",
               },
-              name: 'Disks',
+              name: "Disks",
               showlegend: false,
             }
           ]}
-          layout={ {autosize: true, title: 'Faraday Cage in Two Dimensions'} }
-          style={{ width: '100%', height: '100%' }}
+          layout={{
+            autosize: true, 
+            title: { 
+              text: "Faraday Cage in Two Dimensions",
+              font: {
+                size: 24
+              }
+            },
+            xaxis: {
+              tickfont: {
+                size: 18
+              }
+            },
+            yaxis: {
+              tickfont: {
+                size: 18
+              }
+            },
+            margin: {
+              l: 80,
+              r: 35,
+              t: 100,
+              b: 70
+            }
+          }}
+          style={{ width: "100%", height: "100%" }}
           useResizeHandler={true} 
         />
       </StyledChartContainer>
-      <StyledButton theme={theme} onClick={() => {updateData(n, r, sides)}}>
-        <h4>
-          Update Data
-        </h4>
-      </StyledButton>
+      <StyledButtonContainer>
+        <h3>Settings</h3>
+        <StyledButton theme={theme} onClick={() => {updateData(numDisks, radiusDisks, numSides)}}>
+          <h4>
+            Update Data
+          </h4>
+        </StyledButton>
+        <StyledRowContainer>
+          <StyledIncrementButton theme={theme} onClick={() => {incrementNumDisks("Subtract")}}/>
+          <StyledButton theme={theme}>
+            <h4>
+              Number of Disks
+            </h4>
+            <h4>
+              {numDisks}
+            </h4>
+          </StyledButton>
+          <StyledIncrementButton theme={theme} onClick={() => {incrementNumDisks("Add")}}/>
+        </StyledRowContainer>
+      </StyledButtonContainer>
     </StyledFlexboxContainer>
   )
 }
